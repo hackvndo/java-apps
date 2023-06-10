2f4128e2c863c9311a745c4f48e6e308e3f9eb93
Facundo Tiede <110578724+iamtiedev@users.noreply.github.com>
2023-06-09 23:20:56 -0300

    Update Conexion.java

--- SistemaEstudiantesApp/src/main/java/com/iamtiedev/conexion/Conexion.java
+++ SistemaEstudiantesApp/src/main/java/com/iamtiedev/conexion/Conexion.java
@@ -10,7 +10,7 @@ public class Conexion {
         var baseDatos = "estudiantes_db";
         var url = "jdbc:mysql://localhost:3306/" + baseDatos;
         var usuario = "root";
-        var password = "Mys1.618033988!";
+        var password = "";
 
         // Cargamos la clase del driver de mysql en memoria
         try {
